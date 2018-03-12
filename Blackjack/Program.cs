cf7cff3ce14fff1850b2255fe2a28ca2b09680aa
Exp <theexp111@gmail.com>
2018-03-12 20:44:54 +0100

    Added dealer hand

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -9,11 +9,11 @@ namespace Blackjack
     class Program
     {
         const float winMultiplier = 1.5f;
+        const int minimumBet = 100;
         enum Cards : int
         {
-            ACE = 1,
             TWO = 2,
-            THREE,
+            THREE = 3,
             FOUR,
             FIVE,
             SIX,
@@ -24,34 +24,44 @@ namespace Blackjack
             JACK = 10,
             QUEEN = 10,
             KING = 10,
+            ACE = 11,
         }
 
         static Random random;
         static List<Cards> hand;
         static List<Cards> deck;
-        static int handValue = 0;
         static float money = 1000;
 
-        static void drawCard(int amount = 1)
+        static void drawCard(List<Cards> list, int amount = 1)
         {
             for (int i = 0; i < amount; i++)
             {
-                int at = random.Next(deck.Count - 1);
+                int at = random.Next(deck.Count);
                 Cards card = deck[at];
                 deck.RemoveAt(at); //remove card from deck
-                handValue += (int)card;
-                hand.Add(card);
+                list.Add(card);
             }
         }
 
-        static void printHand()
+        static void printHand(List<Cards> list)
         {
-            Console.WriteLine("Your Hand:");
-            foreach (Cards card in hand)
+            int value = 0;
+            foreach (Cards card in list)
             {
+                value += (int)card;
                 Console.Write(card.ToString() + ", ");
             }
-            Console.WriteLine("Value: " + handValue);
+            Console.WriteLine("Value: " + value);
+        }
+
+        static int getValue(List<Cards> list)
+        {
+            int value = 0;
+            foreach (Cards card in list)
+            {
+                value += (int)card;
+            }
+            return value;
         }
 
         static void Main(string[] args)
@@ -77,16 +87,26 @@ namespace Blackjack
                 hand = new List<Cards>();
                 //Get Dealer hand
                 List<Cards> dealerHand = new List<Cards>();
-                
-                
 
                 //How much money will you sacrifice
-                Console.WriteLine("Enter Bet Money: ");
-                float bet = Convert.ToInt32(Console.ReadLine());
+                float bet = 0;
+                do
+                {
+                    Console.WriteLine("Enter Bet Money (minimum " + minimumBet + "): ");
+                    bet = Convert.ToInt32(Console.ReadLine());
+                } while (bet < minimumBet);
                 money -= bet;
 
-                drawCard(2);
-                printHand();
+                //Draw 
+                //First Dealer
+                drawCard(dealerHand);
+                Console.WriteLine("Dealer: ");
+                printHand(dealerHand);
+
+                //Then player
+                drawCard(hand, 2);
+                Console.WriteLine("Your Hand: ");
+                printHand(hand);
 
                 char cardInput;
                 do
@@ -96,25 +116,49 @@ namespace Blackjack
                     if (cardInput == 'y')
                     {
                         Console.WriteLine("Drawing a new Card...");
-                        drawCard();
-                        printHand();
-                        if (handValue > 21)
+                        drawCard(hand);
+                        Console.WriteLine("Your Hand: ");
+                        printHand(hand);
+                        if (getValue(hand) > 21)
                         {
+                            //Automatically lost
                             Console.WriteLine("You fucked up!");
                             break;
                         }
                     }
-                    else
-                        Console.WriteLine("Unrecognized Command. Draw a new card (y/n)?");
                 } while (cardInput != 'n');
 
-                if (handValue == 21)
+                //Draw for dealer
+                while (getValue(dealerHand) < 17)
                 {
+                    drawCard(dealerHand);
+                }
+                Console.WriteLine("Dealer: ");
+                printHand(dealerHand);
+
+                //Did you win? Prolly not looser
+                int handValue = getValue(hand);
+                int dealerValue = getValue(dealerHand);
+
+                if (handValue > 21) //You're out
+                {
+                    Console.WriteLine("You lost :c!");
+                }
+                else if (dealerValue > 21 || handValue > dealerValue) //yay
+                {
+                    Console.WriteLine("You won!");
                     money += bet * winMultiplier;
+                }
+                else
+                {
+                    if (handValue == dealerValue) //tie
+                        money += bet;
+                    else
+                        Console.WriteLine("You lost :c!");
                 }
 
                 Console.WriteLine("Your Money: " + money);
-                
+
                 do
                 {
                     Console.WriteLine("Want to play again?");
@@ -122,8 +166,6 @@ namespace Blackjack
                 } while (gameInput != 'y' && gameInput != 'n');
 
             } while (gameInput != 'n');
-
-
 
             Console.ReadKey();
         }
